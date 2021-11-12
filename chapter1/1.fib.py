f7773e27d0d4fbc92d4aa7f4a70c3258e4298c8a
omar2205 <omar2205a@yahoo.com>
2021-11-13 01:08:13 +0200

    fib3 Use memoization

--- chapter1/1.fib.py
+++ chapter1/1.fib.py
@@ -1,12 +1,16 @@
 # 1.1 The Fibonacci sequence
+from typing import Dict
+
+# base case
+memo: Dict[int, int] = {0: 0, 1: 1}
 
 def fib(n: int) -> int:
-  # base case
-  if n < 2:
-    return n
-  # recursive case
-  return fib(n - 1) + fib(n - 2)
+  if n not in memo:
+    # memoization
+    memo[n] = fib(n - 1) + fib(n - 2)
+  return memo[n]
 
 # test
 if __name__ == '__main__':
   print(fib(5))
+  print(fib(50))
