40b844cf2c758f09c55a193f8d84a927a9c591a4
omar2205 <omar2205a@yahoo.com>
2021-11-13 01:20:19 +0200

    fib add fib_generator Output the entire Fibonacci sequence up to n

--- chapter1/1.fib.py
+++ chapter1/1.fib.py
@@ -1,4 +1,5 @@
 # 1.1 The Fibonacci sequence
+from typing import Generator
 
 def fib(n: int) -> int:
   # special case
@@ -14,8 +15,26 @@ def fib(n: int) -> int:
 
   return next
 
+def fib_generator(n: int) -> Generator[int, None, None]:
+  """Output the entire Fibonacci sequence to n"""
+  # special cases 0,1
+  yield 0
+  if n > 0: yield 1
+  # same iterative methid as fib
+  last: int = 0
+  next: int = 1
+  
+  for _ in range(1, n):
+    last, next = next, last + next
+    yield next
+
+
 # test
 if __name__ == '__main__':
   print(fib(5))
   print(fib(50))
   print(fib(150))
+
+  # test fib_generator
+  for i in fib_generator(10):
+    print(i)
