2592f04f8bfefc35cea4cb1d90a535a144f3f295
omar2205 <omar2205a@yahoo.com>
2021-11-13 01:16:35 +0200

    fib5 add itrative method

--- chapter1/1.fib.py
+++ chapter1/1.fib.py
@@ -1,18 +1,21 @@
 # 1.1 The Fibonacci sequence
-from functools import lru_cache
 
-# lru_cache causes the return of fib function
-# to be cached every time it's called
-
-@lru_cache(maxsize=None)
 def fib(n: int) -> int:
-  # base case
-  if n < 2:
-    return n
-  # recursive case
-  return fib(n - 1) + fib(n - 2)
+  # special case
+  if n == 0: return n
+  # setting initially to fib(0) and fib(1)
+  last: int = 0
+  next: int = 1
+  
+  for _ in range(1, n):
+    # last = current next
+    # next = prev last + current next
+    last, next = next, last + next
+
+  return next
 
 # test
 if __name__ == '__main__':
   print(fib(5))
   print(fib(50))
+  print(fib(150))
