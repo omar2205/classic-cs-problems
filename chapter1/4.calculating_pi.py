59970ba1a6603f5444b8088e9eddf34beca5e7f7
omar2205 <omar2205a@yahoo.com>
2021-11-13 03:27:42 +0200

    Add 1.4 Calculating Pi

---
+++ chapter1/4.calculating_pi.py
@@ -0,0 +1,18 @@
+# 1.4 Calculating Pi
+# The Leibniz formula:
+# π = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11...
+
+def calculate_pi(n_terms: int) -> float:
+  numerator: float = 4.0
+  denominator: float = 1.0
+  operation: float = 1.0
+  pi: float = 0.0
+  for _ in range(n_terms):
+    pi += operation * (numerator / denominator)
+    denominator += 2.0
+    operation *= -1.0
+  return pi
+
+# test
+if __name__ == '__main__':
+  print(calculate_pi(1000000))
